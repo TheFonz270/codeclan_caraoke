d06033c012af9b24dae578ce5b52af6c977bf476
Martin Quinn <thefonz270@hotmail.com>
2021-10-28 19:02:10 +0100

    Guest class set up and tested.

--- tests/guest_test.py
+++ tests/guest_test.py
@@ -2,8 +2,14 @@ import unittest
 from src.guest import Guest
 
 class TestGuest(unittest.TestCase):
-    def set_up(self):
-        self.customer1("Harry", 50, "Greese Lightning")
+    def setUp(self):
+        self.guest1 = Guest("Harry", 50, "Greese Lightning")
     
     def test_guest_has_name(self):
         self.assertEqual("Harry", self.guest1.name)
+    
+    def test_guest_has_wallet(self):
+        self.assertEqual(50, self.guest1.wallet)
+    
+    def test_guest_has_fav_song(self):
+        self.assertEqual("Greese Lightning", self.guest1.fav_song)
