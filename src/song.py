2c6cd1093fc27c957ee92065d5d7ef5d73fa13c6
Martin Quinn <thefonz270@hotmail.com>
2021-10-28 19:12:26 +0100

    song class set up and tested

--- src/song.py
+++ src/song.py
@@ -1,3 +1,5 @@
 class Song:
-    def __init__(self):
-        pass
+    def __init__(self, name, run_time):
+        self.name = name
+        self.run_time = run_time
+        
--- tests/song_test.py
+++ tests/song_test.py
@@ -2,4 +2,11 @@ import unittest
 from src.song import Song
 
 class TestSong(unittest.TestCase):
-    pass
+    def setUp(self):
+        self.song1 = Song("Crazy Train", 200)
+    
+    def test_song_has_name(self):
+        self.assertEqual("Crazy Train", self.song1.name)
+    
+    def test_song_has_run_time(self):
+        self.assertEqual(200, self.song1.run_time)
